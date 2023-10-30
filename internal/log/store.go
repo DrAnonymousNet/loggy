16028b37f8a763e79c1939aebd6ad17a736625ad
Ahmad Mustapha <18-30gc052@students.unilorin.edu.ng>
2023-10-30 01:14:05 +0100

    test cases for store

--- internal/log/store.go
+++ internal/log/store.go
@@ -11,6 +11,15 @@ var (
 	enc = binary.BigEndian
 )
 
+
+/*ze Metadata: In your code, before writing the actual data p to the Store,
+ you first write the size of the data as metadata. This size metadata indicates 
+ how many bytes of actual data follow it.
+Consistency: To ensure that the size metadata is consistently interpreted,
+ you need to agree on a fixed width for it. 
+ By using lenWidth, you specify that the size metadata is a fixed number of bytes. 
+ This ensures that when you read from the file, you know exactly how many bytes to read to extract the size information.
+*/
 const (
 	lenWidth = 8
 )
@@ -53,17 +62,29 @@ func (s *Store)Append(p []byte)(n uint64, pos uint64, err error){
 }
 
 
-func (s *Store)Read(pos uint64){
-	
-}
-
-func (s *Store)ReadAt(p []byte, off int64 )(int, error){
+func (s *Store)ReadAt(p []byte, off int64)(int ,error){
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	if err := s.buf.Flush(); err!= nil{
+	if err := s.buf.Flush(); err !=nil{
 		return 0, err
 	}
 	return s.File.ReadAt(p, off)
+
+}
+
+func (s *Store)Read(pos uint64)([]byte, error){
+	if err := s.buf.Flush(); err !=nil{
+		return nil, err
+	}
+	size := make([]byte, lenWidth)
+	if _, err := s.File.ReadAt(size, int64(pos)); err != nil{
+		return nil, err
+	}
+	b := make([]byte, enc.Uint64(size))
+	if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil{
+		return nil, err
+	}
+	return b, nil
 }
 
 func (s *Store)Close() error{
